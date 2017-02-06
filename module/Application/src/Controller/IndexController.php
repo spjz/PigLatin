02116a3371e1158bcdfdf2490edee995f79bfec2
PJosepherum <papaverjosepherum@gmail.com>
2017-02-06 13:20:02 +0000

    all done

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -8,12 +8,76 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View;
 use Zend\View\Model\ViewModel;
+use Zend\Http\Request;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+	    $input = htmlentities($this->params()->fromPost('input'));
+
+	    if ($input)
+	    {
+		    return new ViewModel(
+		    	[
+		    		'input' => $input,
+				    'output' => $this->translate($input)
+			    ]
+		    );
+	    }
+	    return new ViewModel();
     }
+
+    private function translate($input)
+    {
+    	$wordEnd = "ay";
+    	$phrase = explode(" ", $input);
+    	$phraseLength = count($phrase);
+
+    	foreach($phrase as $index => $word)
+	    {
+	    	$wordLength = strlen($word);
+	    	$firstVowel = strcspn(strtolower($word), "aeiou");
+	    	$firstLetter = substr(strtolower($word), 0, $firstVowel);
+	    	$punctuation = substr($word, $wordLength - 1, $wordLength);
+
+	    	if ($wordLength > 2)
+		    {
+			    if (preg_match("/(\?|\.|\,|\!)$/", $word))
+			    {
+				    $word = substr($word, 0, $wordLength - 1);
+				    if ($firstVowel > 0)
+				    {
+					    $translation[$index] = substr($word, $firstVowel) . $firstLetter . $wordEnd . $punctuation;
+				    }
+				    else
+				    {
+					    $translation[$index] = $word . $wordEnd . $punctuation;
+				    }
+			    }
+			    else
+			    {
+				    if ($firstVowel > 0)
+				    {
+					    $translation[$index] = substr($word, $firstVowel) . $firstLetter . $wordEnd;
+				    }
+				    else
+				    {
+					    $translation[$index] = $word . $wordEnd;
+				    }
+			    }
+		    }
+		    else
+		    {
+			    $translation[$index] = $word;
+		    }
+
+	    }
+
+	    return $translation;
+
+    }
+
 }
